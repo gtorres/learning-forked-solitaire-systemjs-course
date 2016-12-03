97f8c6cdd6cbaa719f223828bdbaecb9085b0600
Gerard Torres <gm.torres@hotmail.com>
2016-12-03 11:08:15 -0700

    Moved angular scoring boilerplate to klondike.js - separated Scoring function from angular module boilerplate - moved angular boilerplate from scoring.js to klondike.js - exported Scoring function - modified klondike/scoring.js import to explicitly import Scoring function

--- app/klondike/klondike.js
+++ app/klondike/klondike.js
@@ -1,6 +1,9 @@
 import "klondike/board.js";
 import "klondike/game.js";
-import "klondike/scoring.js";
+import { Scoring } from "klondike/scoring.js";
+
+angular.module("klondike.scoring", [])
+  .service("scoring", [Scoring]);
 
 angular.module("klondike", [
   "klondike.game",
